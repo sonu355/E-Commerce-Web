90444e348172053a6a0688f0e3393f14f4c2097c
harshal <harshalc562@gmail.com>
2023-01-29 16:41:49 +0530

    Home page created

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-const slides = document.querySelectorAll(".slide")
-var counter = 0;
-console.log(slides)
-slides.forEach(
-    (slide,index) => {
-        slide.style.left = '$(index * 100)%'
-    }
-)
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
-const goPrev = () => {
-    counter--
-    slideImage() 
-}
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
 
-const goNext = () => {
-    counter++
-    slideImage()
-}
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-const slideImage = () => {
-    slides.forEach(
-        (slide) => {
-            slide.style.transform = 'translateX(-${counter * 100})'
-        }
-    )
-}
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
